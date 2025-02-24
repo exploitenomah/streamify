f3ca56c6f78f5df76688a8176126938e8d4f209e
exploitenomah <iksploiting@gmail.com>
2025-02-24 11:27:23 +0100

    add data for nav links

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
 import "./App.css"
 
 function App() {
-  return <>
-  <img src="/Streamify logo.svg"/>
-  </>
+  return (
+    <>
+      <img src="/Streamify logo.svg" />
+    </>
+  )
 }
 
 export default App
